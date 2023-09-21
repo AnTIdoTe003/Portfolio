3dee43d318ce995301d3e86a2bf6f1f27031c9ba
Debmalya Biswas <68019329+AnTIdoTe003@users.noreply.github.com>
2023-09-21 14:18:51 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -119,6 +119,8 @@ const experiences = [
       "Fixed the invoice API integration.",
       "Fixed several other bugs and created some fresh UI from Figma.",
       "Worked on the Web-Checkin feature.",
+      "Worked on creating a feature known as Pre-Checkin"
+      "Created APIs using GoLang",
     ],
   },
   {
